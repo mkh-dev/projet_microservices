52643bb4fc9e7b720a12ac9ee6b2c7b6cb5acc50
Malek Khelil <malek.kh211@gmail.com>
2024-11-09 15:41:11 +0100

    added api gateway

---
+++ API_Gateway_Server/src/main/java/com/esprit/microservices/api_gateway_server/ApiGatewayServerApplication.java
@@ -0,0 +1,27 @@
+package com.esprit.microservices.api_gateway_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayServerApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("candidat", r -> r.path("/candidats/**")
+                        .uri("http://localhost:8081/"))
+                .route("job", r -> r.path("/job/**")
+                        .uri("http://localhost:8082/"))
+                .build();
+    }
+}
